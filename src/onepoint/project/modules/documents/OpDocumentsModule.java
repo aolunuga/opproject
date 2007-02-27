c88ac60d54e82b2bcbfd4f1ef4cd2f5c806abd12
mcostin <mcostin@f36be67a-51f9-11de-9335-abd5230c83eb>
2007-02-27 14:09:02 +0000

    Removed files from old structure

--- src/onepoint/project/modules/documents/OpDocumentsModule.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright(c) OnePoint Software GmbH 2006. All Rights Reserved.
- */
-
-package onepoint.project.modules.documents;
-
-import onepoint.project.module.OpModule;
-
-public class OpDocumentsModule extends OpModule {}
