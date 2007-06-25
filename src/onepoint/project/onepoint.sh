7cd5eb96673b07b125859fba1e37b29bcef10437
mcostin <mcostin@f36be67a-51f9-11de-9335-abd5230c83eb>
2007-06-25 07:18:36 +0000

    Updated linux scripts.

--- src/onepoint/project/onepoint.sh
+++ src/onepoint/project/onepoint.sh
@@ -1,4 +1,8 @@
-#must have a valid jdk home, please define one if not aldeay defined
-#JAVA_HOME=/usr/local/bin/jdk/bin
 
-$JAVA_HOME/bin/java -jar opproject-07-be.jar
+JAR=opproject-07-be.jar
+
+if [ -e "$JAVA_HOME" ]; then
+    $JAVA_HOME/bin/java -jar $JAR
+else
+    java -jar $JAR
+fi
