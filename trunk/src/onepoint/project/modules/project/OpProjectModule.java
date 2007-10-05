6b663ffb8befad750c30fc752d2e91456a4a4607
cpavel <cpavel@f36be67a-51f9-11de-9335-abd5230c83eb>
2007-10-05 22:13:08 +0000

    Update Source Forge - OPProject module

--- trunk/src/onepoint/project/modules/project/OpProjectModule.java
+++ trunk/src/onepoint/project/modules/project/OpProjectModule.java
@@ -168,4 +168,22 @@ public class OpProjectModule extends OpModule {
       }
       return false;
    }
+
+   /**
+     * Upgrades this module to version #31(via reflection - must be public).
+     *
+     * @param session a <code>OpProjectSession</code> used during the upgrade procedure.
+     */
+    public void upgradeToVersion31(OpProjectSession session) {
+       OpBroker broker = session.newBroker();
+       OpQuery assignmentVersionQuery = broker.newQuery("from OpAssignmentVersion assignmentVersion where assignmentVersion.ActivityVersion is null");
+       OpTransaction tx = broker.newTransaction();
+       Iterator<OpAssignmentVersion> assignmentsIt = broker.iterate(assignmentVersionQuery);
+       while (assignmentsIt.hasNext()) {
+          OpAssignmentVersion assignmentVersion = assignmentsIt.next();
+          broker.deleteObject(assignmentVersion);
+       }
+       tx.commit();
+       broker.close();
+    }   
 }
