e182f07c76eb8bd7a537b7d179b27f4b5c040d1a
cpavel <cpavel@f36be67a-51f9-11de-9335-abd5230c83eb>
2007-11-30 00:06:22 +0000

    Update Source Forge - OPProject module

--- src/onepoint/persistence/hibernate/OpHibernateSource.java
+++ src/onepoint/persistence/hibernate/OpHibernateSource.java
@@ -55,7 +55,7 @@ public class OpHibernateSource extends OpSource {
    /**
     * The latest schema version
     */
-   public static final int SCHEMA_VERSION = 34;
+   public static final int SCHEMA_VERSION = 35;
 
    /**
     * Db schema related constants
