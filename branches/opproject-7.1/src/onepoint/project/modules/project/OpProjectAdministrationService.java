eeca1ca620a2e43c2856df933ab66aa98f0ef9f3
cpavel <cpavel@f36be67a-51f9-11de-9335-abd5230c83eb>
2008-01-08 00:08:23 +0000

    Update Source Forge - OPProject module

--- branches/opproject-7.1/src/onepoint/project/modules/project/OpProjectAdministrationService.java
+++ branches/opproject-7.1/src/onepoint/project/modules/project/OpProjectAdministrationService.java
@@ -527,22 +527,21 @@ public class OpProjectAdministrationService extends OpProjectService {
          OpProjectStatus status = null;
          if (statusLocator != null && !statusLocator.equals(NULL_ID)) {
             status = (OpProjectStatus) (broker.getObject(statusLocator));
+         }
+         else {
+            status = null;
+         }
+         if ((status == null && project.getStatus() != null) ||
+              (status != null && project.getStatus() == null) ||
+              (status != null && project.getStatus() != null && project.getStatus().getID() != status.getID())) {
+
+            //check access level
             if ((projectAccesssLevel != OpPermission.ADMINISTRATOR) && !(status.equals(project.getStatus()))) {
                error = session.newError(ERROR_MAP, OpProjectError.NO_RIGHTS_CHANGING_STATUS_ERROR);
                reply.setError(error);
                return reply;
             }
-
             project.setStatus(status);
-         }
-         else {
-            if ((projectAccesssLevel != OpPermission.ADMINISTRATOR) && (project.getStatus() != null)) {
-               error = session.newError(ERROR_MAP, OpProjectError.NO_RIGHTS_CHANGING_STATUS_ERROR);
-               reply.setError(error);
-               return reply;
-            }
-
-            project.setStatus(null);
          }
 
          OpProjectPlan projectPlan = project.getPlan();
