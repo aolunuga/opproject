eb2caddc082b1f2c2802f6618e9a0498d623968e
dfreis <dfreis@f36be67a-51f9-11de-9335-abd5230c83eb>
2008-12-19 14:13:50 +0000

    Update SourceForge - OPProject module

--- trunk/demodata/createdb.sql
+++
@@ -1,2 +0,0 @@
-create database opproject;
-grant all privileges on opproject.* to 'opproject'@'localhost' identified by 'opproject' with grant option;
--- trunk/src/onepoint/project/modules/documents/OpDocumentsModule.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright(c) OnePoint Software GmbH 2007. All Rights Reserved.
- */
-
-package onepoint.project.modules.documents;
-
-import onepoint.project.module.OpModule;
-
-public class OpDocumentsModule extends OpModule {}
